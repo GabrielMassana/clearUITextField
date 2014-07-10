69c94dbae65793872deadc956ab4860ae3467613
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-07-10 11:56:51 +0100

    Initial Commit

---
+++ clearUITextField/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  clearUITextField
+//
+//  Created by Gabriel Massana on 10/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ clearUITextField/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  clearUITextField
+//
+//  Created by Gabriel Massana on 10/07/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
